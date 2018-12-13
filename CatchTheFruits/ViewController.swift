cd030ad2c677e32bb6d081b0d660da9b02c2dc99
Halil Özel <halilozel1903@gmail.com>
2018-12-13 18:47:24 +0300

    Initial Commit

---
+++ CatchTheFruits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchTheFruits
+//
+//  Created by Halil Özel on 13.12.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
